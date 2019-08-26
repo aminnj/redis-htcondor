93e966bc3abd437d4e4352cc63e5243266055703
Nick Amin <amin.nj@gmail.com>
2019-08-25 19:11:40 -0700

    funky behavior. we dont rely on it, so take it out

--- tests/utils_t.py
+++ tests/utils_t.py
@@ -16,16 +16,6 @@ class UtilsTest(unittest.TestCase):
         fprime = decompress_and_loads(compressed)
         self.assertEqual(fprime(1), 2)
 
-    def test_serialization_global(self):
-        a = 1
-
-        def f(x):
-            return x+a
-        compressed = compress_and_dumps(f)
-        a = 2
-        fprime = decompress_and_loads(compressed)
-        self.assertEqual(fprime(1), 3)
-
 
 if __name__ == "__main__":
     unittest.main()
